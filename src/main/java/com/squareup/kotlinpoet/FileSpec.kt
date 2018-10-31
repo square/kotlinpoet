3c6174f4adbba39fc4b9bad8f79b30d602bdb334
Shaishav Gandhi <shaishgandhi@gmail.com>
2018-10-31 05:43:48 -0700

    Add support for multi-line file comments (#514)
    * Add support for multiline file comments
    
    * Add extension to check if CodeBlock is multi-line

--- src/main/java/com/squareup/kotlinpoet/FileSpec.kt
+++ src/main/java/com/squareup/kotlinpoet/FileSpec.kt
@@ -87,7 +87,11 @@ class FileSpec private constructor(builder: FileSpec.Builder) {
 
   private fun emit(codeWriter: CodeWriter) {
     if (comment.isNotEmpty()) {
-      codeWriter.emitComment(comment)
+      if (comment.isMultiLine()) {
+        codeWriter.emitKdoc(comment)
+      } else {
+        codeWriter.emitComment(comment)
+      }
     }
 
     if (annotations.isNotEmpty()) {
@@ -129,6 +133,10 @@ class FileSpec private constructor(builder: FileSpec.Builder) {
     }
 
     codeWriter.popPackage()
+  }
+
+  private fun CodeBlock.isMultiLine(): Boolean {
+    return this.toString().contains("\n")
   }
 
   override fun equals(other: Any?): Boolean {
@@ -209,6 +217,10 @@ class FileSpec private constructor(builder: FileSpec.Builder) {
 
     fun addComment(format: String, vararg args: Any) = apply {
       comment.add(format, *args)
+    }
+
+    fun addComment(comment: CodeBlock) = apply {
+      this.comment.add(comment)
     }
 
     fun addType(typeSpec: TypeSpec) = apply {
