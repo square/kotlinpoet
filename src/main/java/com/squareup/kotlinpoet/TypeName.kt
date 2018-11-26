332b7a4e3886eebfde94848b44a6dfb084dcd6b2
Zac Sweers <zsweers@uber.com>
2018-11-25 18:05:21 -0800

    Make TypeName a sealed class

--- src/main/java/com/squareup/kotlinpoet/TypeName.kt
+++ src/main/java/com/squareup/kotlinpoet/TypeName.kt
@@ -61,7 +61,7 @@ import kotlin.reflect.KClass
  * types like `Set<Long>`, use the factory methods on [ParameterizedTypeName], [TypeVariableName],
  * and [WildcardTypeName].
  */
-abstract class TypeName internal constructor(
+sealed class TypeName internal constructor(
   val nullable: Boolean, annotations: List<AnnotationSpec>
 ) {
   val annotations = annotations.toImmutableList()
