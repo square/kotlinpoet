55486637abe0e7b558617c5ce1917ce132584f49
Zac Sweers <pandanomic@gmail.com>
2019-03-13 19:00:27 -0700

    Initial pass at OriginatingElement interface

---
+++ src/main/java/com/squareup/kotlinpoet/OriginatingElementsHolder.kt
@@ -0,0 +1,27 @@
+package com.squareup.kotlinpoet
+
+import javax.lang.model.element.Element
+
+/** A type that can have originating [elements][Element]. */
+interface OriginatingElementsHolder {
+
+  /** The originating elements of this type. */
+  val originatingElements: List<Element>
+
+  /** The builder analogue to [OriginatingElementsHolder] types. */
+  interface Builder<out T : Builder<T>> {
+
+    /** Mutable map of the current originating elements this builder contains. */
+    val originatingElements: MutableList<Element>
+
+    /** Adds an [originatingElement] to this type's list of originating elements. */
+    @Suppress("UNCHECKED_CAST")
+    fun addOriginatingElement(originatingElement: Element): T = apply {
+      originatingElements += originatingElement
+    } as T
+  }
+}
+
+internal fun OriginatingElementsHolder.Builder<*>.buildOriginatingElements(): OriginatingElementsHolder = OriginatingElements(originatingElements.toImmutableList())
+
+internal class OriginatingElements(override val originatingElements: List<Element>) : OriginatingElementsHolder
