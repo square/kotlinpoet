7da1dc362c38ba330a0dd5cffcd4de370f657dc3
Zac Sweers <zac.sweers@gmail.com>
2019-06-21 16:50:03 -0500

    Set up multi-module project structure
    To enable adding new artifacts after this. Note that to preserve git history best, it'd be ideal to merge this commit(s) directly as-is

---
+++ kotlinpoet/build.gradle.kts
@@ -0,0 +1,56 @@
+/*
+ * Copyright (C) 2019 Square, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import org.jetbrains.dokka.gradle.DokkaTask
+
+plugins {
+  kotlin("jvm")
+  id("org.jetbrains.dokka")
+  id("com.vanniktech.maven.publish") version "0.8.0"
+}
+
+val GROUP: String by project
+val VERSION_NAME: String by project
+
+group = GROUP
+version = VERSION_NAME
+
+tasks.named<Jar>("jar") {
+  manifest {
+    attributes("Automatic-Module-Name" to "com.squareup.kotlinpoet")
+  }
+}
+
+afterEvaluate {
+  tasks.named<DokkaTask>("dokka") {
+    skipDeprecated = true
+    outputFormat = "html"
+  }
+}
+
+dependencies {
+  api("org.jetbrains.kotlin:kotlin-stdlib-jdk7")
+  implementation("org.jetbrains.kotlin:kotlin-reflect")
+  testImplementation("org.jetbrains.kotlin:kotlin-test-junit")
+  testImplementation("com.google.truth:truth:0.42")
+  testImplementation("com.google.testing.compile:compile-testing:0.15")
+  testImplementation("com.google.jimfs:jimfs:1.1")
+  testImplementation("org.eclipse.jdt.core.compiler:ecj:4.6.1")
+}
+
+repositories {
+  mavenCentral()
+  jcenter()
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -14,24 +14,6 @@
  * limitations under the License.
  */
 
-plugins {
-  kotlin("jvm") version "1.3.31" apply false
-  id("org.jetbrains.dokka") version "0.9.18" apply false
-  id("com.diffplug.gradle.spotless") version "3.22.0"
-}
-
-spotless {
-  kotlin {
-    target("**/*.kt")
-    ktlint("0.31.0").userData(mapOf("indent_size" to "2"))
-    trimTrailingWhitespace()
-    endWithNewline()
-  }
-}
-
-subprojects {
-  repositories {
-    mavenCentral()
-    jcenter()
-  }
-}
+include(
+    ":kotlinpoet"
+)
