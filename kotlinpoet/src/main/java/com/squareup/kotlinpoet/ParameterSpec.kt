30b44b847b9a1e671d6cea1a1441bb6d3345dd2e
Louis Wasserman <lowasser@google.com>
2019-07-03 10:45:06 -0700

    Add a simple constructor for ParameterSpec
    Fixes #717.

--- kotlinpoet/src/main/java/com/squareup/kotlinpoet/ParameterSpec.kt
+++ kotlinpoet/src/main/java/com/squareup/kotlinpoet/ParameterSpec.kt
@@ -26,6 +26,9 @@ class ParameterSpec private constructor(
   builder: ParameterSpec.Builder,
   private val tagMap: TagMap = builder.buildTagMap()
 ) : Taggable by tagMap {
+  
+  constructor(name: String, type: TypeName, vararg modifiers: KModifier): this(builder(name, type, *modifiers))
+  
   val name = builder.name
   val kdoc = builder.kdoc.build()
   val annotations = builder.annotations.toImmutableList()
