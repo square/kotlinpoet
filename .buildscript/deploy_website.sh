a92b9a032e8b39d732c47a35caae925f709cf83c
Shaishav Gandhi <shaishgandhi@gmail.com>
2019-07-04 22:53:53 -0700

    Clean up unnecessary bits

--- .buildscript/deploy_website.sh
+++ .buildscript/deploy_website.sh
@@ -8,7 +8,7 @@
 
 set -ex
 
-REPO="git@github.com:ShaishavGandhi/kotlinpoet.git"
+REPO="git@github.com:square/kotlinpoet.git"
 DIR=temp-clone
 
 # Delete any existing temporary website clone
@@ -19,12 +19,9 @@ git clone $REPO $DIR
 
 # Move working directory into temp folder
 cd $DIR
-# Temporary
-git checkout sg/docs
 
 # Generate the API docs
 ./gradlew :kotlinpoet:dokka
-
 
 # Copy in special files that GitHub wants in the project root.
 cat README.md > docs/index.md
@@ -33,12 +30,6 @@ cp CONTRIBUTING.md docs/contributing.md
 
 # Build the site and push the new files up to GitHub
 mkdocs gh-deploy
-
-## Restore Javadocs from 1.x, 2.x, and 3.x.
-#git checkout gh-pages
-#git cherry-pick bb229b9dcc9a21a73edbf8d936bea88f52e0a3ff
-#git cherry-pick c695732f1d4aea103b826876c077fbfea630e244
-#git push
 
 # Delete our temp folder
 cd ..
