ea46b9c3aacb0ec192319d2eaeae6375969a8b88
Zac Sweers <zac.sweers@gmail.com>
2019-07-08 12:21:28 -0400

    Start TypeNameKotlinTest with typeNameOf() tests

---
+++ kotlinpoet/src/test/java/com/squareup/kotlinpoet/TypeNameKotlinTest.kt
@@ -0,0 +1,60 @@
+package com.squareup.kotlinpoet
+
+import com.google.common.truth.Truth.assertThat
+import org.junit.Test
+
+@ExperimentalStdlibApi
+class TypeNameKotlinTest {
+
+  @Test
+  fun typeNameOf_simple() {
+    val type = typeNameOf<TypeNameKotlinTest>()
+    assertThat(type.toString()).isEqualTo("com.squareup.kotlinpoet.TypeNameKotlinTest")
+  }
+
+  @Test
+  fun typeNameOf_simple_intrinsic() {
+    val type = typeNameOf<String>()
+    assertThat(type.toString()).isEqualTo("kotlin.String")
+  }
+
+  @Test
+  fun typeNameOf_array_primitive() {
+    val type = typeNameOf<IntArray>()
+    assertThat(type.toString()).isEqualTo("kotlin.IntArray")
+  }
+
+  @Test
+  fun typeNameOf_array_parameterized() {
+    val type = typeNameOf<Array<String>>()
+    assertThat(type.toString()).isEqualTo("kotlin.Array<kotlin.String>")
+  }
+
+  @Test
+  fun typeNameOf_generic() {
+    val type = typeNameOf<List<String>>()
+    assertThat(type.toString()).isEqualTo("kotlin.collections.List<kotlin.String>")
+  }
+
+  @Test
+  fun typeNameOf_generic_wildcard_out() {
+    val type = typeNameOf<GenericType<out String>>()
+    assertThat(type.toString()).isEqualTo("com.squareup.kotlinpoet.TypeNameKotlinTest.GenericType<out kotlin.String>")
+  }
+
+  @Test
+  fun typeNameOf_generic_wildcard_in() {
+    val type = typeNameOf<GenericType<in String>>()
+    assertThat(type.toString()).isEqualTo("com.squareup.kotlinpoet.TypeNameKotlinTest.GenericType<in kotlin.String>")
+  }
+
+  @Test
+  fun typeNameOf_complex() {
+    val type = typeNameOf<Map<String, List<Map<Int, GenericType<in Set<Array<GenericType<out String>>>>>>>>()
+    assertThat(type.toString()).isEqualTo("kotlin.collections.Map<kotlin.String, kotlin.collections.List<kotlin.collections.Map<kotlin.Int, com.squareup.kotlinpoet.TypeNameKotlinTest.GenericType<in kotlin.collections.Set<kotlin.Array<com.squareup.kotlinpoet.TypeNameKotlinTest.GenericType<out kotlin.String>>>>>>>")
+  }
+
+  @Suppress("unused")
+  class GenericType<T>
+
+}
