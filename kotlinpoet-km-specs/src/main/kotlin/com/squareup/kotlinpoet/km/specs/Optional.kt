a6e1975315def2b65a7850401006be4246ad1855
Zac Sweers <zac.sweers@gmail.com>
2019-08-11 21:05:16 -0400

    delete now-unused optional

--- kotlinpoet-km-specs/src/main/kotlin/com/squareup/kotlinpoet/km/specs/Optional.kt
+++
@@ -1,9 +0,0 @@
-package com.squareup.kotlinpoet.km.specs
-
-/**
- * Simple `Optional` implementation for use in collections that don't allow `null` values.
- *
- * TODO: Make this an inline class when inline classes are stable.
- */
-internal data class Optional<out T : Any>(val nullableValue: T?)
-internal fun <T : Any> T?.toOptional(): Optional<T> = Optional(this)
