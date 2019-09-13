e4c0d43de75f4c915c58c84a77460869f31065b1
Zac Sweers <zac.sweers@gmail.com>
2019-09-13 03:14:12 -0400

    Create FieldData

---
+++ kotlinpoet-metadata-specs/src/main/kotlin/com/squareup/kotlinpoet/metadata/specs/FieldData.kt
@@ -0,0 +1,33 @@
+package com.squareup.kotlinpoet.metadata.specs
+
+import com.squareup.kotlinpoet.AnnotationSpec
+import com.squareup.kotlinpoet.AnnotationSpec.UseSiteTarget.FIELD
+import com.squareup.kotlinpoet.CodeBlock
+import com.squareup.kotlinpoet.metadata.KotlinPoetMetadataPreview
+
+@KotlinPoetMetadataPreview
+data class FieldData(
+    private val annotations: List<AnnotationSpec>,
+    val isSynthetic: Boolean,
+    val jvmModifiers: Set<JvmFieldModifier>,
+    val constant: CodeBlock?
+) {
+
+  val allAnnotations = ElementHandler.createAnnotations(
+      FIELD) {
+    addAll(annotations)
+    if (isSynthetic) {
+      add(JVM_SYNTHETIC_SPEC)
+    }
+    addAll(jvmModifiers.map { it.annotationSpec() })
+  }
+
+  companion object {
+    val SYNTHETIC = FieldData(
+        annotations = emptyList(),
+        isSynthetic = true,
+        jvmModifiers = emptySet(),
+        constant = null
+    )
+  }
+}
