8f3bb604b89470e4098196e7e351b0f50944a7b5
Zac Sweers <zac.sweers@gmail.com>
2019-09-13 03:13:58 -0400

    Fixup with jvmmodifier

--- kotlinpoet-metadata-specs/src/main/kotlin/com/squareup/kotlinpoet/metadata/specs/JvmModifier.kt
+++ kotlinpoet-metadata-specs/src/main/kotlin/com/squareup/kotlinpoet/metadata/specs/JvmModifier.kt
@@ -1,7 +1,9 @@
 package com.squareup.kotlinpoet.metadata.specs
 
 import com.squareup.kotlinpoet.AnnotationSpec
+import com.squareup.kotlinpoet.metadata.KotlinPoetMetadataPreview
 
+@KotlinPoetMetadataPreview
 interface JvmModifier {
   fun annotationSpec(): AnnotationSpec
 }
