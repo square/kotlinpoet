b9c359d29f9516b4cd4b166bc1d68275dca26563
Zac Sweers <pandanomic@gmail.com>
2019-09-20 03:12:19 -0400

    Update kotlinpoet-metadata-specs/src/main/kotlin/com/squareup/kotlinpoet/metadata/specs/ClassInspector.kt
    Co-Authored-By: Egor Andreevich <andreevich.egor@gmail.com>

--- kotlinpoet-metadata-specs/src/main/kotlin/com/squareup/kotlinpoet/metadata/specs/ClassInspector.kt
+++ kotlinpoet-metadata-specs/src/main/kotlin/com/squareup/kotlinpoet/metadata/specs/ClassInspector.kt
@@ -20,7 +20,7 @@ import com.squareup.kotlinpoet.metadata.ImmutableKmClass
 import com.squareup.kotlinpoet.metadata.KotlinPoetMetadataPreview
 import kotlinx.metadata.jvm.JvmMethodSignature
 
-/** A basic interface for looking up information about JVM information about a given Class. */
+/** A basic interface for looking up JVM information about a given Class. */
 @KotlinPoetMetadataPreview
 interface ClassInspector {
 
