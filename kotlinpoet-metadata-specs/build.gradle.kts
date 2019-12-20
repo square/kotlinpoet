f364539d0c1d10d530732a78ff1a878caac9936f
Egor Andreevici <egor@squareup.com>
2019-12-20 11:25:30 -0500

    Dokka 0.10.0

--- kotlinpoet-metadata-specs/build.gradle.kts
+++ kotlinpoet-metadata-specs/build.gradle.kts
@@ -36,7 +36,8 @@ tasks.named<Jar>("jar") {
 
 afterEvaluate {
   tasks.named<DokkaTask>("dokka") {
-    skipDeprecated = true
+    // TODO(egorand): Re-enable when https://github.com/Kotlin/dokka/issues/512 is fixed
+    // skipDeprecated = true
     outputFormat = "html"
   }
 }
