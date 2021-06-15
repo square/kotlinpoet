5d1fc21ad720fe1de937aafee168ebb00e567e46
Goooler <wangzongler@gmail.com>
2021-06-15 22:35:03 +0800

    Update dependencies (#1092)
    * Update dependencies
    
    * Bump gradle to 7.1
    
    * Bump spotless to 5.13.0
    
    * Revert mavenPublish to 0.13.0

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -15,14 +15,14 @@
  */
 object versions {
   const val kotlin = "1.5.10"
-  const val spotless = "5.12.5"
+  const val spotless = "5.13.0"
   const val ktlint = "0.41.0"
   const val mavenPublish = "0.13.0"
   const val dokka = "1.4.32"
 }
 
 object deps {
-  const val autoCommon = "com.google.auto:auto-common:1.0"
+  const val autoCommon = "com.google.auto:auto-common:1.0.1"
   const val guava = "com.google.guava:guava:30.1.1-jre"
   object kotlin {
     const val reflect = "org.jetbrains.kotlin:kotlin-reflect:${versions.kotlin}"
@@ -34,6 +34,6 @@ object deps {
     const val compileTesting = "com.google.testing.compile:compile-testing:0.19"
     const val jimfs = "com.google.jimfs:jimfs:1.2"
     const val ecj = "org.eclipse.jdt.core.compiler:ecj:4.6.1"
-    const val kotlinCompileTesting = "com.github.tschuchortdev:kotlin-compile-testing:1.4.1"
+    const val kotlinCompileTesting = "com.github.tschuchortdev:kotlin-compile-testing:1.4.2"
   }
 }
