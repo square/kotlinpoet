25b07fe333efa764d0bfa2d5e984f95126a31d54
Zac Sweers <pandanomic@gmail.com>
2021-09-17 21:35:28 -0400

    Nix old comment
    Can't be a value class anyway since it's nullable

--- interop/kotlinx-metadata/specs/src/main/kotlin/com/squareup/kotlinpoet/metadata/classinspectors/Optional.kt
+++ interop/kotlinx-metadata/specs/src/main/kotlin/com/squareup/kotlinpoet/metadata/classinspectors/Optional.kt
@@ -2,8 +2,6 @@ package com.squareup.kotlinpoet.metadata.classinspectors
 
 /**
  * Simple `Optional` implementation for use in collections that don't allow `null` values.
- *
- * TODO: Make this an inline class when inline classes are stable.
  */
 internal data class Optional<out T : Any>(val nullableValue: T?)
 
