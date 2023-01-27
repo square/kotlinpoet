27ce14a2dad4920bb2d9d6031f2866f66799d985
Egor Andreevich <github@egorand.dev>
2023-01-27 21:23:23 +0100

    Integrate foojay-resolver and re-enable tests on all JDK versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -101,16 +101,11 @@ subprojects {
   // Copied from https://github.com/square/retrofit/blob/master/retrofit/build.gradle#L28.
   // Create a test task for each supported JDK.
   for (majorVersion in 8..18) {
-    // Adoptium JDK 9 cannot extract on Linux or Mac OS.
-    if (majorVersion == 9) continue
-    // Started causing build failures in late 2022, e.g.:
-    // https://github.com/square/kotlinpoet/actions/runs/3816320722/jobs/6531532305.
-    if (majorVersion == 10) continue
-
     val jdkTest = tasks.register<Test>("testJdk$majorVersion") {
       val javaToolchains = project.extensions.getByType(JavaToolchainService::class)
       javaLauncher.set(javaToolchains.launcherFor {
         languageVersion.set(JavaLanguageVersion.of(majorVersion))
+        vendor.set(JvmVendorSpec.AZUL)
       })
 
       description = "Runs the test suite on JDK $majorVersion"
