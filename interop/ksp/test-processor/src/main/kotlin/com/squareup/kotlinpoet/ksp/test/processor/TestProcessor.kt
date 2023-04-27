c2f0810fd95af3383d9ccb08822765a56e096223
Zac Sweers <zac.sweers@gmail.com>
2023-04-27 15:24:55 -0400

    Fix arguments handling in KSType.toTypeName() (#1529)
    * Add supertypes and classes to TestProcessor outputs
    
    * Revert odd arguments switch
    
    * Add a few regression tests
    
    * Don't make superclass positional

--- interop/ksp/test-processor/src/main/kotlin/com/squareup/kotlinpoet/ksp/test/processor/TestProcessor.kt
+++ interop/ksp/test-processor/src/main/kotlin/com/squareup/kotlinpoet/ksp/test/processor/TestProcessor.kt
@@ -22,8 +22,10 @@ import com.google.devtools.ksp.isConstructor
 import com.google.devtools.ksp.processing.Resolver
 import com.google.devtools.ksp.processing.SymbolProcessor
 import com.google.devtools.ksp.processing.SymbolProcessorEnvironment
+import com.google.devtools.ksp.symbol.ClassKind
 import com.google.devtools.ksp.symbol.KSAnnotated
 import com.google.devtools.ksp.symbol.KSClassDeclaration
+import com.squareup.kotlinpoet.ANY
 import com.squareup.kotlinpoet.FileSpec
 import com.squareup.kotlinpoet.FunSpec
 import com.squareup.kotlinpoet.ParameterSpec
@@ -65,6 +67,32 @@ class TestProcessor(private val env: SymbolProcessorEnvironment) : SymbolProcess
           decl.annotations
             .filterNot { it.shortName.getShortName() == "ExampleAnnotation" }
             .map { it.toAnnotationSpec() }.asIterable(),
+        )
+        val allSupertypes = decl.superTypes.toList()
+        val (superclassReference, superInterfaces) = if (allSupertypes.isNotEmpty()) {
+          val superClass = allSupertypes.firstOrNull {
+            val resolved = it.resolve()
+            resolved is KSClassDeclaration && resolved.classKind == ClassKind.CLASS
+          }
+          if (superClass != null) {
+            superClass to allSupertypes.filterNot { it == superClass }
+          } else {
+            null to allSupertypes
+          }
+        } else {
+          null to allSupertypes
+        }
+
+        superclassReference?.let {
+          val typeName = it.toTypeName(decl.typeParameters.toTypeParameterResolver())
+          if (typeName != ANY) {
+            superclass(typeName)
+          }
+        }
+        addSuperinterfaces(
+          superInterfaces.map { it.toTypeName(decl.typeParameters.toTypeParameterResolver()) }
+            .filterNot { it == ANY }
+            .toList(),
         )
       }
     val classTypeParams = decl.typeParameters.toTypeParameterResolver()
