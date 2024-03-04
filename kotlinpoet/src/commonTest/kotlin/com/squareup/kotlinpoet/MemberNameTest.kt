c1c1f1135907ed958ac46237138b43db32b8aba8
Jake Wharton <jw@squareup.com>
2024-03-04 09:29:24 -0500

    Simplify test case logic

--- kotlinpoet/src/commonTest/kotlin/com/squareup/kotlinpoet/MemberNameTest.kt
+++ kotlinpoet/src/commonTest/kotlin/com/squareup/kotlinpoet/MemberNameTest.kt
@@ -323,58 +323,41 @@ class MemberNameTest {
   }
 
   @Test fun importedMemberClassFunctionNameDontClashForParameterValue() {
-    fun createBuildFunc(params: List<String>): FunSpec {
-      val tacoClassname = ClassName.bestGuess("com.squareup.tacos.Taco")
-      val bodyCodeBlock = CodeBlock.builder()
-        .add("return %T", tacoClassname)
-        .apply {
-          val paramsBlock = params.map { paramName ->
-            val paramValue = ClassName(
-              packageName = "com.squareup",
-              simpleNames = listOf("Fridge"),
-            ).member("meat")
-            CodeBlock.of("$paramName = %L", CodeBlock.of("%M { }", paramValue))
-          }
-            .map { block -> CodeBlock.of("%L", block) }
-            .joinToCode(prefix = "(", suffix = ")")
-          add(paramsBlock)
-        }
-        .build()
-      return FunSpec.builder("build")
-        .returns(tacoClassname)
-        .addCode(bodyCodeBlock)
-        .build()
-    }
-
-    val spec = FileSpec.builder("com.squareup.tacos", "Tacos")
+    val tacoName = ClassName("com.squareup.tacos", "Taco")
+    val meatMember = ClassName("com.squareup", "Fridge").member("meat")
+    val buildFun = FunSpec.builder("build")
+      .returns(tacoName)
+      .addStatement("return %T(%M { })", tacoName, meatMember)
+      .build()
+    val spec = FileSpec.builder(tacoName)
       .addType(
         TypeSpec.classBuilder("DeliciousTaco")
-          .addFunction(createBuildFunc(listOf("deliciousMeat")))
+          .addFunction(buildFun)
           .addFunction(FunSpec.builder("deliciousMeat").build())
           .build(),
       )
       .addType(
         TypeSpec.classBuilder("TastelessTaco")
-          .addFunction(createBuildFunc(listOf("meat")))
+          .addFunction(buildFun)
           .addFunction(FunSpec.builder("meat").build())
           .build(),
       )
-    val source = spec.build()
-    assertThat(source.toString()).isEqualTo(
+      .build()
+    assertThat(spec.toString()).isEqualTo(
       """
       |package com.squareup.tacos
       |
       |import com.squareup.Fridge.meat
       |
       |public class DeliciousTaco {
-      |  public fun build(): Taco = Taco(deliciousMeat = meat { })
+      |  public fun build(): Taco = Taco(meat { })
       |
       |  public fun deliciousMeat() {
       |  }
       |}
       |
       |public class TastelessTaco {
-      |  public fun build(): Taco = Taco(meat = com.squareup.Fridge.meat { })
+      |  public fun build(): Taco = Taco(com.squareup.Fridge.meat { })
       |
       |  public fun meat() {
       |  }
