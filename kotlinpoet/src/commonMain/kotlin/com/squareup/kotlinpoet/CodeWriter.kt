8eed20b3e0ab20f1c39833fd40c0e9f50d25533f
ForteScarlet <ForteScarlet@163.com>
2024-08-29 13:55:17 +0800

    Clean up the standalone `Closeable` type and implement `kotlin.AutoCloseable` directly instead.

--- kotlinpoet/src/commonMain/kotlin/com/squareup/kotlinpoet/CodeWriter.kt
+++ kotlinpoet/src/commonMain/kotlin/com/squareup/kotlinpoet/CodeWriter.kt
@@ -62,7 +62,7 @@ internal class CodeWriter(
   private val importedTypes: Map<String, ClassName> = emptyMap(),
   private val importedMembers: Map<String, Set<MemberName>> = emptyMap(),
   columnLimit: Int = 100,
-) : Closeable {
+) : AutoCloseable {
   private var out = LineWrapper(out, indent, columnLimit)
   private var indentLevel = 0
 
--- kotlinpoet/src/commonMain/kotlin/com/squareup/kotlinpoet/LineWrapper.kt
+++ kotlinpoet/src/commonMain/kotlin/com/squareup/kotlinpoet/LineWrapper.kt
@@ -24,7 +24,7 @@ internal class LineWrapper(
   private val out: Appendable,
   private val indent: String,
   private val columnLimit: Int,
-) : Closeable {
+) : AutoCloseable {
 
   private var closed = false
 
