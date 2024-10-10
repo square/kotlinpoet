765a6c21251ca5bd96741e9016fd34ba72c86af2
Egor Andreevich <egor@squareup.com>
2024-10-10 14:53:09 -0700

    Bump Java versions for build and tests

--- build.gradle.kts
+++ build.gradle.kts
@@ -116,7 +116,7 @@ subprojects {
     fun Project.setupCheckTask(testTaskName: String) {
       // Copied from https://github.com/square/retrofit/blob/master/retrofit/build.gradle#L28.
       // Create a test task for each supported JDK. We check every "LTS" + current version.
-      val versionsToTest = listOf(8, 11, 17, 19)
+      val versionsToTest = listOf(8, 11, 17, 21, 22)
       for (majorVersion in versionsToTest) {
         val jdkTest = tasks.register<Test>("testJdk$majorVersion") {
           val javaToolchains = project.extensions.getByType(JavaToolchainService::class)
