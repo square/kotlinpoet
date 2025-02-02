d8a1484c83880632f4f1e5c36752badd761a1dbd
Prashanth Rao <iamprashanthrao@gmail.com>
2025-02-02 14:33:49 +0530

    Aliased import ignored for nullable variants of that type (#2068)
    * Aliased import ignored for nullable variants of that type
    
    * fix for build failure
    
    * Apply suggestions from code review
    
    Co-authored-by: Jake Wharton <github@jakewharton.com>
    Co-authored-by: Egor Andreevich <github@egorand.dev>
    
    * fix for build failure
    
    ---------
    
    Co-authored-by: Jake Wharton <github@jakewharton.com>
    Co-authored-by: Egor Andreevich <github@egorand.dev>

--- docs/changelog.md
+++ docs/changelog.md
@@ -5,6 +5,7 @@ Change Log
 
 * Fix: Support delegates on anonymous classes. (#2034)
 * Fix: `TypeName.MUTABLE_MAP_ENTRY` now correctly uses the `MutableEntry` nested class name. (#2061)
+* Fix: Use the same aliased import for both the nullable and non-nullable versions of a type (#2020 #2021)
 
 
 ## Version 2.0.0
--- kotlinpoet/src/jvmMain/kotlin/com/squareup/kotlinpoet/CodeWriter.kt
+++ kotlinpoet/src/jvmMain/kotlin/com/squareup/kotlinpoet/CodeWriter.kt
@@ -517,7 +517,7 @@ internal class CodeWriter(
       val newImportTypes = if (alias == null) {
         topLevelClassName
       } else {
-        className
+        className.copy(nullable = false) as ClassName
       }
       importableTypes[simpleName] = importableTypes.getValue(simpleName) + newImportTypes
     }
