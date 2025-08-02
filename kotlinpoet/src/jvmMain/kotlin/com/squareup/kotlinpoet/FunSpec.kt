df638fdb679bf7114e83fa49753bf198ae82c7cf
Mina <144630931+mina-jaff@users.noreply.github.com>
2025-08-02 14:41:53 +0200

    Fix FunSpec.beginControlFlow to accept nullable args
    - Change parameter type from vararg args: Any to vararg args: Any?
    - This makes it consistent with CodeBlock.beginControlFlow
    - Add test case to verify null arguments work correctly
    
    Fixes #2174

--- kotlinpoet/src/jvmMain/kotlin/com/squareup/kotlinpoet/FunSpec.kt
+++ kotlinpoet/src/jvmMain/kotlin/com/squareup/kotlinpoet/FunSpec.kt
@@ -543,7 +543,7 @@ public class FunSpec private constructor(
      * @param controlFlow the control flow construct and its code, such as "if (foo == 5)".
      * * Shouldn't contain braces or newline characters.
      */
-    public fun beginControlFlow(controlFlow: String, vararg args: Any): Builder = apply {
+    public fun beginControlFlow(controlFlow: String, vararg args: Any?): Builder = apply {
       body.beginControlFlow(controlFlow, *args)
     }
 
